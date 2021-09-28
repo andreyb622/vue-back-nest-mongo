c542d4357ac544251e4d6c80aaaf5f289dbb8286
andreyb622 <andreyb622@gmail.com>
2021-09-28 16:17:41 +0300

    fixed tslint setting

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const globalPrefix: string = 'api';
+
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
-	app.setGlobalPrefix('api');
-	app.enableCors();
-	await app.listen(3000);
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix(globalPrefix);
+  app.enableCors();
+  await app.listen(3000);
 }
 bootstrap();
--- src/user/user.model.ts
+++ src/user/user.model.ts
@@ -4,12 +4,12 @@ import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
 export interface UserModel extends Base {}
 
 export class UserModel extends TimeStamps {
-	@prop({ unique: true })
-	email: string;
+  @prop({ unique: true })
+  email: string;
 
-	@prop()
-	passwordHash: string;
+  @prop()
+  passwordHash: string;
 
-	@prop({ default: 'user' })
-	role: string;
+  @prop({ default: 'user' })
+  role: string;
 }
