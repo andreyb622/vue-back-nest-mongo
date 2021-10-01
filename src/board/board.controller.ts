efffdf5050c00985fef2dffaba3412f71edbc863
andreyb622 <andreyb622@gmail.com>
2021-10-01 18:01:19 +0300

    added guards in board controller

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -6,7 +6,9 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 import { BoardService } from './board.service';
 import { BoardDto } from './dto/board.dto';
 
@@ -18,21 +20,26 @@ export class BoardController {
     return this.boardService.findBoard(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get('/')
   async getAll() {
     return this.boardService.findAllBoards();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post('/')
   async create(@Body() dto: BoardDto) {
     return this.boardService.createBoard(dto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   async patch(@Param('id') id: string, @Body() dto: BoardDto) {
     const updateBoard = await this.boardService.updateBoardById(id, dto);
     return updateBoard;
   }
+
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   async delete(@Param('id') id: string) {
     return this.boardService.deleteBoardById(id);
