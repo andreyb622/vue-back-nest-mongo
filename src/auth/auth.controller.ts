cf73df2297bc2595346dc71dc8d71b866ac20203
andreyb622 <andreyb622@gmail.com>
2021-10-01 17:29:21 +0300

    fixed dto and added register/create user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,38 +1,32 @@
 import {
-	BadRequestException,
-	Body,
-	Controller,
-	HttpCode,
-	Post,
-	UsePipes,
-	ValidationPipe,
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
-import { ALREADY_REGISTERED_ERROR } from './auth.constants';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
-	constructor(
-		private readonly authService: AuthService,
-		private readonly userService: UserService,
-	) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
 
-	@UsePipes(new ValidationPipe())
-	@Post('register')
-	async register(@Body() dto: AuthDto) {
-		const oldUser = await this.userService.findUser(dto.email);
-		if (oldUser) {
-			throw new BadRequestException(ALREADY_REGISTERED_ERROR);
-		}
-		return this.authService.createUser(dto);
-	}
-
-	@HttpCode(200)
-	@Post('login')
-	async login(@Body() { email, password }: AuthDto) {
-		const user = await this.authService.validateUser(email, password);
-		return this.authService.login(user.email);
-	}
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Post('login')
+  async login(@Body() dto: AuthDto) {
+    const oldUser = await this.userService.findUser(dto.email);
+    if (oldUser) {
+      const user = await this.authService.validateUser(dto.email, dto.password);
+      return this.authService.login(user.email);
+    }
+    const newUser = await this.authService.createUser(dto);
+    return this.authService.login(newUser.email);
+  }
 }
--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,9 +1,9 @@
-import { IsString } from 'class-validator';
+import { IsEmail, IsString } from 'class-validator';
 
 export class AuthDto {
-	@IsString()
-	email: string;
+  @IsEmail()
+  email: string;
 
-	@IsString()
-	password: string;
+  @IsString()
+  password: string;
 }
